732f89c9fb7183e4dcdbe41365d3f0e155a9d3a5
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-22 22:18:08 -0500

    finished last two exercises

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Some software developers were contracted to build some code, but there weren't quite clear yet on how scope works. Your job to fix their code to stop all the error from being thrown.
+// C IF FOR COOKIE
 
 const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 let y = 1
@@ -8,7 +8,7 @@ for (let x = 0; x < cookies.length; x++) {
   console.log(`Mmmmmmm.... that's a good ${currentCookie} cookie`)
 }
 
-// 
+// CONJUNCTION FUNCTION
 
 const conjunction = function (firstWord, secondWord) {
   let conjoinedWord = `${firstWord} ${secondWord}`
@@ -18,8 +18,7 @@ const conjunction = function (firstWord, secondWord) {
 let conjoinedWord = conjunction("Master", "Card")
 console.log(conjoinedWord)
 
-// 
-
+// MOD SQUAD
 
   const ModSquad = {
       "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
@@ -38,6 +37,49 @@ console.log(conjoinedWord)
   let container = document.querySelector(".show-info")
   container.innerHTML = HTMLRepresentation
 
+  // SIMON SAYS
+  // There are two separate scoping issues in this code. Remember, for and if are their own block scopes and if something is declared with let or const then that variable only exists in that scope.
+  // TODO See about refactoring with total invalid declared inside loop
   
+  const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+  let totalInvalid = 0
+  
+  for (let k = 0; k < locations.length; k++) {
+    let currentLocation = locations[k]
+    if (currentLocation[0] > 2) {
+      console.log("That location is invalid")
+      totalInvalid += 1
+    } 
+}
+console.log(`There were ${totalInvalid} invalid locations`)
+
+
+
+// The llamaNamer function defines an internal function and your job is to rewrite this code so that the internal function gets returned.
+
+
+
+const llamaNamer = () => {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = () => {
+        let suffix = " the Llama"
+        let name = possibleNames[randomizer]
+        return name + suffix
+    }
+    return namer
+}
+
+const nameMaker = llamaNamer()
+console.log(nameMaker)
+
+
+  
+
+
+
+
+
 
 
